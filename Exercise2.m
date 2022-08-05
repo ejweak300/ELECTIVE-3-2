a4887cc9aa6d691229bb11840611a8d7169d27e8
ejweak300 <97329828+ejweak300@users.noreply.github.com>
2022-08-05 22:40:46 +0800

    Add files via upload

---
+++ Exercise2.m
@@ -0,0 +1,15 @@
+pkg load image
+a=imread('fruits.png');
+imfinfo('fruits.png')
+imshow(a);
+
+b=imresize(a,0.5);
+imwrite(b,'fruits2.png');
+imfinfo('fruits2.png')
+figure(2),imshow(b);
+
+hsv_a=rgb2hsv(a);
+imwrite(hsv_a,'fruits3.png');
+figure(3),imshow(hsv_a);
+
+%The data type of image fruits is png
